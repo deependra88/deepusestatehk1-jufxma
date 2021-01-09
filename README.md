0b2434bc51ee892f56d80a6c16365349a4c8b397
deependra88 <deependra.gaurav@gmail.com>
2021-01-09 22:40:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# deepusestatehk1-jufxma
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/deepusestatehk1-jufxma)
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+//import ClassCounter from "./components/ClassCounter";
+//import HookCounter from "./components/HookCounter";
+//import ClassCounterTwo from "./components/ClassCounterTwo";
+//import HookCounterTwo from "./components/HookCounterTwo";
+//import HookCounterThree from "./components/HookCounterThree";
+//import HookCounterFour from "./components/HookCounterFour";
+//import ClassCounterOne from "./components/ClassCounterOne";
+//import HookCounterOne from "./components/HookCounterOne";
+//import ClassMouse from "./components/ClassMouse";
+//import HookMouse from "./components/HookMouse";
+//import MouseContainer from "./components/MouseContainer";
+//import IntervalClassCounter from "./components/IntervalClassCounter";
+//import IntervalHookCounter from "./components/IntervalHookCounter";
+import DataFetching from "./components/DataFetching";
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter /> */}
+      {/*<HookCounter /> */}
+      {/*<ClassCounterTwo />*/}
+      {/* <HookCounterTwo /> */}
+      {/*<HookCounterThree /> */}
+      {/*<HookCounterFour /> */}
+      {/*<ClassCounterOne />*/}
+      {/*<HookCounterOne />*/}
+      {/*<ClassMouse />*/}
+      {/*<HookMouse />*/}
+      {/*<MouseContainer />*/}
+      {/*<IntervalClassCounter />*/}
+      {/*<IntervalHookCounter /> */}
+      <DataFetching />
+    </div>
+  );
+}
+
+export default App;
